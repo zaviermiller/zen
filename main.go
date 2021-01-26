4f2bf0ae2e1d9e8717fc762a08196ad0baa6f236
Zavier <zavier@resaleai.com>
2021-01-26 13:22:56 -0500

    clean up and comment

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 // to know when an input causes newline
 
 type inputDetector struct {
-	value  bool
 	update func()
 }
 
@@ -27,25 +26,30 @@ type inputSubject struct {
 	observers []inputDetector
 }
 
+// register a new detector to listen to the subject
 func (s *inputSubject) register(i inputDetector) {
 	s.observers = append(s.observers, i)
 }
 
+// remove all listeners (easy to implement and all i needed really)
 func (s *inputSubject) unregisterAll() {
 	s.observers = []inputDetector{}
 }
 
+// notify all detectors of a change
 func (s inputSubject) notifyInput() {
 	for _, detector := range s.observers {
 		detector.detected()
 	}
 }
 
+// the detected func runs the passed update func from initialization,
+// in order to have access to necessary data
 func (d *inputDetector) detected() {
-	d.value = true
 	d.update()
 }
 
+// enum type for different outputs
 type outputType int
 
 const (
@@ -59,97 +63,104 @@ type output struct {
 	content    string
 }
 
+// comparison of two outputs returning a diff object
 func (o output) compareDiff(o1 output) *zenDiff {
+	// display constant
 	om := []string{"PROMPT", "RESPONSE"}
+
+	// check to make sure the output types are the same (worst case if outputTypes dont match)
 	if o.outputType != o1.outputType {
 		return &zenDiff{errorType: "OUTPUT TYPE", testDiff: om[o1.outputType], correctDiff: om[o.outputType]}
 	}
 
-	// fDiffChan := make(chan int)
-
-	// go func() {
-	// 	for i, char := range o.content {
-	// 		if char != rune(o1.content[i]) {
-	// 			// return &zenDiff{errorType: om[o1.outputType] + " -- CONTENT DIFF", testDiff: o1.content[i:], correctDiff:  }
-	// 			fDiffChan <- i
-	// 		}
-	// 	}
-	// }()
-
-	// bDiff := 0
-
+	// basic forward checking of output to ensure match
+	// probably need to improve
 	for i, char := range o.content {
 		if char != rune(o1.content[i]) {
-			// bDiff := i
-			// break
 			return &zenDiff{errorType: om[o1.outputType] + " CONTENT", testDiff: o1.content[i:], correctDiff: o.content[i:]}
 		}
 	}
 
-	// fDiff := <-fDiffChan
-
-	// if
+	// lines matched
 	return nil
 }
 
+// difference object that contains data for printing
+// the diffs to the screen
 type zenDiff struct {
 	testDiff    string
 	correctDiff string
 	errorType   string
 }
 
-func (z zenDiff) print(id int, correctBinPath, testBinPath string) {
+// prints out the diff
+func (z zenDiff) print(correctId, testId int, correctBinPath, testBinPath string) {
 	fmt.Println(u.Red + u.Bright + u.Clear + "!!!" + z.errorType + " DIFF!!! " + u.Normal + "\n")
-	fmt.Println("   " + correctBinPath + " [" + strconv.Itoa(id) + "]: " + u.Green + z.correctDiff + u.Normal)
-	fmt.Println("   " + testBinPath + " [" + strconv.Itoa(id) + "]: " + u.Red + z.testDiff + u.Normal + "\n")
+	fmt.Println("   " + correctBinPath + " [" + strconv.Itoa(correctId) + "]: " + u.Green + z.correctDiff + u.Normal)
+	fmt.Println("   " + testBinPath + " [" + strconv.Itoa(testId) + "]: " + u.Red + z.testDiff + u.Normal + "\n")
 }
 
 func main() {
 
+	// ensure correct usage
 	if len(os.Args) < 3 {
 		check(errors.New("Usage: " + u.Purple + "zen [example binary] [your binary]" + u.Normal))
 	}
+
+	// create variables from args
 	correctBinPath := os.Args[1]
 	testBinPath := os.Args[2]
 	binOpts := os.Args[3:]
 
+	// inputs and correct outputs store
 	inputs := []string{}
 	outputs := []output{}
 
+	// input notifier
 	inpSubject := inputSubject{}
 
-	// writer := bufio.NewWriter(os.Stdout)
-
+	// pretty stuff
 	fmt.Println(u.Green + u.Bright + u.Zen)
 	fmt.Println(u.Purple + "\n   \"Bring yourself peace\" -Z" + u.Normal + "\n")
-	fmt.Println("[" + u.Purple + "*" + u.Normal + "] Executing correct lab file (you may need to enter input)...\n")
+	zenLogLn("Executing correct lab file (you may need to enter input)...\n")
 
+	// build correct lab cmd and io pipes
 	cmd := exec.Command(correctBinPath, binOpts...)
 	cmd.Stderr = os.Stderr
-	// cmd.Stdout = os.Stdout
-	// cmd.Stdin = os.Stdin
+
 	execIn, err := cmd.StdinPipe()
 	check(err)
 	execOut, err := cmd.StdoutPipe()
 	check(err)
 
+	// create the output scanner and scan every byte (char)
 	scanner := bufio.NewScanner(execOut)
 	scanner.Split(bufio.ScanBytes)
 
+	// async function to read the output
 	go func() {
+
+		// keep track of output
 		tmpString := ""
 		fmt.Print("   ")
-		inputSignal := inputDetector{value: false, update: func() {
+
+		// create and register input detector to break off output of type PROMPT on input
+		inputSignal := inputDetector{update: func() {
 			outputs = append(outputs, output{outputType: PROMPT, content: tmpString})
+
+			// reset
 			tmpString = ""
 			fmt.Print("   ")
 		}}
 		inpSubject.register(inputSignal)
+
+		// begin scanning output
 		for scanner.Scan() {
 			text := scanner.Text()
 			tmpString += text
 			fmt.Print(text)
 
+			// break and reset output of type RESPONSE when new line
 			if text == "\n" {
 				outputs = append(outputs, output{outputType: RESPONSE, content: tmpString})
 				tmpString = ""
@@ -158,47 +169,66 @@ func main() {
 		}
 	}()
 
+	// start running cmd
 	err = cmd.Start()
 	check(err)
 
+	// async function to read input
 	go func() {
+
+		// stdin reader
 		reader := bufio.NewReader(os.Stdin)
+
+		// read on 'enter'
 		input, err := reader.ReadString('\n')
 		for err == nil {
+			// notify input
 			inpSubject.notifyInput()
+
+			// add input to input list
 			inputs = append(inputs, input)
 			n, err := io.WriteString(execIn, input)
 			if err != nil || n == 0 {
+				// err means no more input needed
 				break
 			}
 			input, err = reader.ReadString('\n')
 		}
+
+		// close input pipe
 		execIn.Close()
 	}()
 
+	// wait for cmd to finish
 	cmd.Wait()
 
+	// reset notifier
 	inpSubject.unregisterAll()
 
+	// track test exec time
 	t1 := time.Now()
 
 	// Start test execution
-	fmt.Print("\n\r[" + u.Purple + "*" + u.Normal + "] Execution finished! Executing test lab...")
+	zenLog("Execution finished! Executing test lab...")
 
+	// outputs of test execution
 	testOutputs := []output{}
 
+	// build test lab cmd and pipes
 	cmd = exec.Command(testBinPath, binOpts...)
 	testIn, err := cmd.StdinPipe()
 	check(err)
 	testOut, err := cmd.StdoutPipe()
 	check(err)
 
+	// build new scanner for output pipe
 	scanner = bufio.NewScanner(testOut)
 	scanner.Split(bufio.ScanBytes)
 
+	// async output, same as above
 	go func() {
 		tmpString := ""
-		inputSignal := inputDetector{value: false, update: func() {
+		inputSignal := inputDetector{update: func() {
 			testOutputs = append(testOutputs, output{outputType: PROMPT, content: tmpString})
 			tmpString = ""
 		}}
@@ -214,25 +244,32 @@ func main() {
 		}
 	}()
 
+	// run test lab
 	err = cmd.Start()
 	check(err)
 
+	// do each input with time between
 	for _, input := range inputs {
 		time.Sleep(300 * time.Millisecond)
 		inpSubject.notifyInput()
 		io.WriteString(testIn, input)
 	}
 
+	// wait for lab test to finish
 	cmd.Wait()
 
+	// show test exec is done
 	fmt.Println(u.Bright + "Done!" + u.Normal + "\n")
 
+	// begin showing comp loader
 	printLoader(0, max(len(testOutputs), len(outputs)))
 
+	// diff vars
 	diffCount := 0
 	testIndex := 0
 	correctIndex := 0
 
+	// go through diffs
 	for correctIndex < len(outputs) {
 		if testIndex >= len(testOutputs) {
 			break
@@ -245,7 +282,7 @@ func main() {
 			for i := 1; i < 3; i++ {
 				if testIndex+i < len(testOutputs) {
 					if output.content == testOutputs[testIndex+i].content {
-						zenDiff{errorType: "LINE", correctDiff: "", testDiff: testOutput.content}.print(correctIndex, correctBinPath, testBinPath)
+						zenDiff{errorType: "LINE", correctDiff: "", testDiff: testOutput.content}.print(correctIndex, testIndex, correctBinPath, testBinPath)
 						diffCount++
 						testIndex += i
 						testOutput = testOutputs[testIndex]
@@ -255,7 +292,7 @@ func main() {
 
 				if correctIndex+i < len(outputs) {
 					if outputs[i+correctIndex].content == testOutputs[testIndex].content {
-						zenDiff{errorType: "LINE", correctDiff: output.content, testDiff: ""}.print(correctIndex, correctBinPath, testBinPath)
+						zenDiff{errorType: "LINE", correctDiff: output.content, testDiff: ""}.print(correctIndex, testIndex, correctBinPath, testBinPath)
 						diffCount++
 						correctIndex += i
 						output = outputs[correctIndex]
@@ -270,31 +307,33 @@ func main() {
 
 		if diff != nil {
 			diffCount++
-			diff.print(correctIndex, correctBinPath, testBinPath)
+			diff.print(correctIndex, testIndex, correctBinPath, testBinPath)
 		}
 
 		testIndex++
 		correctIndex++
 		time.Sleep(10 * time.Millisecond)
 		printLoader(max(correctIndex, testIndex), max(len(testOutputs), len(outputs)))
-
-		// loaderProgress <- max(testIndex, correctIndex)
 	}
 
+	// get elapsed time
 	t2 := time.Now()
 	elapsed := t2.Sub(t1)
 
-	fmt.Println(fmt.Sprintf("\n\n["+u.Purple+"*"+u.Normal+"] Finished comparing files in %s", elapsed))
+	fmt.Println()
+	zenLogLn(fmt.Sprintf("Finished comparing files in %s", elapsed))
 
 	if diffCount == 0 {
-		fmt.Println(u.Bright + u.Green + "\nNO MAJOR DIFFERENCES DETECTED! 100% GOOD JOB!\n" + u.Normal)
+		fmt.Println(u.Bright + u.Green + "\nNO DIFFERENCES DETECTED! 100% GOOD JOB!\n" + u.Normal)
 		return
 	}
 
-	fmt.Println("\n[" + u.Purple + "*" + u.Normal + "] Score: " + colorScore(float64(len(outputs)-diffCount)/float64(len(outputs))) + "\n")
+	// show score
+	zenLogLn("Score: " + colorScore(float64(len(outputs)-diffCount)/float64(len(outputs))) + "\n")
 
 }
 
+// basic error checking function
 func check(err error) {
 	if err != nil {
 		fmt.Println(u.Bright + u.Red + "ZEN ERROR: " + u.Normal + err.Error())
@@ -302,6 +341,7 @@ func check(err error) {
 	}
 }
 
+// max of two nums
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -309,6 +349,7 @@ func max(a, b int) int {
 	return b
 }
 
+// prints loader
 func printLoader(done, total int) {
 	width := 50.0
 
@@ -318,6 +359,7 @@ func printLoader(done, total int) {
 	fmt.Print(fmt.Sprintf("["+u.Purple+"*"+u.Normal+"] Finding diffs... [ "+equals+dashes+" ] [%d/%d compared] - (%f", int(done), int(total), math.Round(progress*100.0*100.0)/100.0) + "%) \r")
 }
 
+// computes color of score
 func colorScore(score float64) string {
 	textScore := fmt.Sprintf("%.2f", score)
 	if score >= .75 {
@@ -327,4 +369,13 @@ func colorScore(score float64) string {
 	} else {
 		return u.Red + textScore + u.Normal
 	}
+}
+
+// log helpers
+func zenLog(msg string) {
+	fmt.Print("\n\r[" + u.Purple + "*" + u.Normal + "] " + msg)
+}
+
+func zenLogLn(msg string) {
+	fmt.Println("\n\r[" + u.Purple + "*" + u.Normal + "] " + msg)
 }
